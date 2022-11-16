36bd3538cf57aadb38f17a1bcde27a9ab7f8c653
Carine SEMWAGA <semwagacarine@gmail.com>
2022-11-16 17:52:23 +0200

    add deposit and loan pages

---
+++ src/components/Payment/Deposit.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FormLayout from "./Form";
+
+const Deposit = () => {
+  return <FormLayout />;
+};
+
+export default Deposit;
---
+++ src/components/Payment/Loan.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import FormLayout from "./Form";
+
+const Loan = () => {
+  return <FormLayout isLoan />;
+};
+
+export default Loan;
