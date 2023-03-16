882ee18c8e3c15e142be39504cb6bf11f09f5c79
Carine SEMWAGA <51264308+Krasivaya@users.noreply.github.com>
2023-03-16 19:11:33 +0200

    add a new spreadsheet for each new user

--- src/apis/createSheet.js
+++ src/apis/createSheet.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { dataProperties } from "../constant/sheetProperties";
 
-const createSheet = async ({ user, token }) => {
+const createSheet = async ({ token }) => {
   await axios
     .post(
       `https://sheets.googleapis.com/v4/spreadsheets?key=${process.env.REACT_APP_API_KEY}`,
@@ -35,6 +35,44 @@ const createSheet = async ({ user, token }) => {
     .then((res) =>
       localStorage.setItem("spreadsheetId", res.data.spreadsheetId)
     )
+    .catch((err) => console.log(err));
+};
+
+export const createSheetForUser = async ({ user, token }) => {
+  await axios
+    .post(
+      `https://sheets.googleapis.com/v4/spreadsheets?key=${process.env.REACT_APP_API_KEY}`,
+      {
+        properties: {
+          title: `${user}`,
+        },
+        sheets: [
+          {
+            properties: {
+              title: "Deposit",
+            },
+            data: dataProperties.deposit,
+          },
+          {
+            properties: {
+              title: "Loan",
+            },
+            data: dataProperties.loan,
+          },
+        ],
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${
+            process.env.REACT_APP_GOOGLE_TOKEN || token
+          }`,
+        },
+      }
+    )
+    .then((res) => {
+      localStorage.setItem("spreadsheetId", res.data.spreadsheetId);
+      return res;
+    })
     .catch((err) => console.log(err));
 };
 
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -3,20 +3,65 @@ import { Card, notification } from "antd";
 import { useNavigate } from "react-router-dom";
 import GoogleLogin from "react-google-login";
 import { gapi } from "gapi-script";
+import moment from "moment";
 import "./index.scss";
 import { DISCOVERY_DOCS, SCOPES } from "../../constant/gapi";
-// import createSheet from "../../apis/createSheet";
+import createSheet, { createSheetForUser } from "../../apis/createSheet";
+import { getAllUsers } from "../../apis/getSheet";
+import updateSheet from "../../apis/updateSheet";
 
 const Login = () => {
+  const spreadsheetId = process.env.REACT_APP_OVERALL_SHEET_ID;
   const navigate = useNavigate();
 
-  const onSuccess = (response) => {
-    localStorage.setItem("access_token", response.accessToken);
-    localStorage.setItem("current_user", response.profileObj.name);
-    localStorage.setItem("current_user_email", response.profileObj.email);
-    // createSheet({ user: response.wt.Ad, token: response.accessToken });
+  const onSuccess = async (response) => {
+    const {
+      accessToken: token,
+      profileObj: { name, email },
+    } = response;
+    updateLocalStorage({ token, name, email });
+
+    const isAdmin = checkIfUserIsAdmin(email);
+    const hasLoggedIn = await checkIfUserLoggedIn(email);
+
+    if (!hasLoggedIn) {
+      if (isAdmin) await createSheet({ token });
+      else await createSheetForUser({ user: name, token });
+
+      await addLoggedInUser({ name, email });
+    }
 
     return navigate("/");
+  };
+
+  const checkIfUserIsAdmin = (email) => {
+    const adminEmails = [process.env.REACT_APP_ADMIN_EMAIL];
+    return adminEmails.includes(email);
+  };
+
+  const checkIfUserLoggedIn = async (email) => {
+    const users = await getAllUsers({
+      spreadsheetId,
+      ranges: "Users!B2:B1000",
+    });
+
+    return users.includes(email);
+  };
+
+  const addLoggedInUser = async ({ name, email }) => {
+    const userSpreadsheetId = localStorage.getItem("spreadsheetId");
+
+    await updateSheet({
+      spreadsheetId,
+      range: "Users!A2:D2",
+      values: [[name, email, userSpreadsheetId, moment().format("lll")]],
+    });
+  };
+
+  const updateLocalStorage = ({ token, name, email }) => {
+    localStorage.setItem("access_token", token);
+    localStorage.setItem("current_user", name);
+    localStorage.setItem("current_user_email", email);
   };
 
   const onFailure = (response) => {
